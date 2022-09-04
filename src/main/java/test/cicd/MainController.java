a1386f83b7ada82158e5588595150c8f877026e8
Sangin <ksiisk99@gmail.com>
2022-09-04 19:02:06 +0900

    first commit

---
+++ src/main/java/test/cicd/MainController.java
@@ -0,0 +1,12 @@
+package test.cicd;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping("/")
+    public String main(){
+        return "Hello World";
+    }
+}
