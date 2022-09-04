43f5805a2701c3dfa3177856266bae34f88f1193
Sangin <ksiisk99@gmail.com>
2022-09-04 19:59:35 +0900

    build test

--- src/main/java/test/cicd/MainController.java
+++ src/main/java/test/cicd/MainController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @GetMapping("/")
     public String main(){
-        return "Hello World";
+        return "Hello World2";
     }
 }
