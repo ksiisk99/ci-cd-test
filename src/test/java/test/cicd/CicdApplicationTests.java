ac8c9ba641d875869589288a2f42b20f4ee1cf73
Sangin <ksiisk99@gmail.com>
2022-09-04 20:50:24 +0900

    build error test

--- src/test/java/test/cicd/CicdApplicationTests.java
+++ src/test/java/test/cicd/CicdApplicationTests.java
@@ -1,13 +1,22 @@
 package test.cicd;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class CicdApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void test() {
+		List<Integer>list=new ArrayList<>();
+
+		assertThat(list.isEmpty()).isFalse();
 	}
 
 }
